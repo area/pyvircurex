f7b34fe7c7a577388646e05e1c3746cf1525503e
Christopher Poole <mail@christopherpoole.net>
2013-04-05 14:02:28 +1000

    added support for secure requests of account balance

--- vircurex/account.py
+++ vircurex/account.py
@@ -1,10 +1,34 @@
+import random
+
+from common import secure_request, make_token
+
+
+tokens = {
+    "get_balance" : {
+        # secret;user;timestamp;ID;get_balance;currency
+        "input" : "%s;%s;%s;%i;get_balance;%s",
+        # secret;user;timestamp;get_balance;balance
+        "output" : "%s;%s;%s;get_balance;%s" 
+    },
+    "get_balances" : {
+        # secret;user;timestamp;ID;get_balance
+        "input" : "%s;%s;%s;%i;get_balances",
+        # secret;user;timestamp;get_balance;balance
+        "output" : "%s;%s;%s;get_balances;%s" 
+    }
+}
+
 
 class Account(object):
-    def __init__(self):
-        pass
+    def __init__(self, user, secret):
+        self.user = user
+        self.secret = secret
+        self.tid = random.randint(0, 2**32)
 
-    def balance(self, curency):
-        pass
+    def balance(self, currency):
+        return secure_request(self, "balance",
+                tokens["get_balance"], ("currency",), (currency,))
+
 
     def balances(self):
         pass
--- vircurex/common.py
+++ vircurex/common.py
@@ -1,6 +1,8 @@
+import time
 import json
 import urllib
 import httplib
+import hashlib
 
 
 domain = "vircurex.com"
@@ -37,11 +39,16 @@ api_schema = {
     "trades" : {
         "url" : "/api/trades.json",
         "type" : list 
+    },
+    "balance" : {
+        "url" : "/api/get_balance.json",
+        "return" : "balance",
+        "type" : float
     }
 }
 
 
-def request(api_call, params={}):
+def make_request(api_call, params):
     api = api_schema[api_call]
      
     params = urllib.urlencode(params)
@@ -52,12 +59,52 @@ def request(api_call, params={}):
     response = connection.getresponse().read()
     connection.close()
     
-    data = json.loads(response)
-    
+    return json.loads(response)
+   
+   
+def request(api_call, params={}):
+    api = api_schema[api_call]
+    data = make_request(api_call, params)
+
     if api.has_key("return"):
         value = data[api["return"]]
     else:
         value = data
     
     return api["type"](value)
+
+
+def secure_request(account, api_call, token_string, names, params):
+    api = api_schema[api_call]
+    stamp, token = make_token(account, token_string["input"], params)
+    request_params = {
+        "account" : account.user,
+        "id" : account.tid,
+        "token" : token,
+        "timestamp" : stamp,
+    }
+    request_params.update(zip(names, params))
+
+    data = make_request(api_call, request_params)
+    print data
+
+    print check_token(account, data["timestamp"], token_string["output"], (data["balance"],))
+  
+    return api["type"](data["balance"])
+
+     
+def make_token(account, token_string, params):
+    stamp = time.strftime("%Y-%m-%dT%H:%M:%S", tuple(time.gmtime()))
+    params = tuple([account.secret, account.user, stamp, account.tid] + list(params))
+
+    token = hashlib.sha256(token_string % params).hexdigest()
+    return stamp, token
+
+
+def check_token(account, stamp, token_string, params):
+    params = tuple([account.secret, account.user, stamp] + list(params))
+
+    print token_string % params
+    token = hashlib.sha256(token_string % params).hexdigest()
+    return  token
 
